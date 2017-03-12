30bb73530bd7c21ff77858361cb7a2be91d949e5
Andrii <whooles96@gmail.com>
2017-03-12 11:43:36 +0100

    TestFixex

--- lab2/src/test/java/pl/edu/pjwstk/lab2/lab2/VectorTest.java
+++ lab2/src/test/java/pl/edu/pjwstk/lab2/lab2/VectorTest.java
@@ -3,12 +3,16 @@ package pl.edu.pjwstk.lab2.lab2;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.Ignore;
 import junit.framework.Assert;
-import junit.framework.Test;
 import junit.framework.TestCase;
+
 
 public class VectorTest {
 
+	@Test
 	public void New2ElementsVectorCreatingNoException(){
 		VectorImpl testVector = new VectorImpl();
 		List<Integer> vector = new ArrayList<Integer>(); 
@@ -17,6 +21,7 @@ public class VectorTest {
 		testVector.setVector(vector);
 	}
 	
+	@Test(expected = RuntimeException.class)
 	public void NewEmptyVectorCreatingExceptionThrown(){
 		VectorImpl testVector = new VectorImpl();
 		List<Integer> emptyList = new ArrayList<Integer>(); 
@@ -42,6 +47,7 @@ public class VectorTest {
 		
 	}
 	
+	@Test(expected = RuntimeException.class)
 	public void addVectorToCurrentWithDifferentSizesExceptionThrown(){
 		VectorImpl testVector = new VectorImpl();
 		VectorImpl testVector1 = new VectorImpl();
@@ -80,6 +86,7 @@ public class VectorTest {
 		testVectorResult.addVectors(testVector, testVector1);	
 	}
 	
+	@Test(expected = RuntimeException.class)
 	public void addTwoVectorsWithDefferentSizesExceptionThrown(){
 		VectorImpl testVector = new VectorImpl();
 		VectorImpl testVector1 = new VectorImpl();
@@ -118,7 +125,8 @@ public class VectorTest {
 		testVector.sub(testVector1);		
 	}
 	
-public void subVectorFromCurrentDefferentSizesExceptionThrown(){
+	@Test(expected = RuntimeException.class)
+	public void subVectorFromCurrentDefferentSizesExceptionThrown(){
 		
 		VectorImpl testVector = new VectorImpl();
 		VectorImpl testVector1 = new VectorImpl();
@@ -136,6 +144,46 @@ public void subVectorFromCurrentDefferentSizesExceptionThrown(){
 		testVector1.setVector(list1);
 		
 		testVector.sub(testVector1);		
+	}
+
+	public void subTwoVectors(){
+		VectorImpl testVector = new VectorImpl();
+		VectorImpl testVector1 = new VectorImpl();
+		VectorImpl testVectorResult = new VectorImpl();
+	
+		List<Integer> vector = new ArrayList<Integer>(); 
+		vector.add(2);
+		vector.add(6);
+	
+		List<Integer> vector1 = new ArrayList<Integer>(); 
+		vector1.add(5);
+		vector1.add(1);
+	
+		testVector.setVector(vector);
+		testVector1.setVector(vector1);
+	
+		testVectorResult.subVectors(testVector, testVector1);	
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void subTwoVectorsWithDefferentSizesExceptionThrown(){
+		
+		VectorImpl testVector = new VectorImpl();
+		VectorImpl testVector1 = new VectorImpl();
+		VectorImpl testVectorResult = new VectorImpl();
+	
+		List<Integer> list = new ArrayList<Integer>(); 
+		list.add(2);
+		list.add(6);
+	
+		List<Integer> list1 = new ArrayList<Integer>(); 
+		list1.add(5);
+		list1.add(1);
+	
+		testVector.setVector(list);
+		testVector1.setVector(list1);
+	
+		testVectorResult.subVectors(testVector, testVector1);	
 	}
 	
 	public void getVector(){
