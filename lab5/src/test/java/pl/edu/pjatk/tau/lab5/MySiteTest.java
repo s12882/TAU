a95e63c5f64969473cb30a49db32a04848e8f19d
Andrii <whooles96@gmail.com>
2017-04-03 17:25:21 +0200

    Tests wlaśnie

--- lab5/src/test/java/pl/edu/pjatk/tau/lab5/MySiteTest.java
+++ lab5/src/test/java/pl/edu/pjatk/tau/lab5/MySiteTest.java
@@ -17,44 +17,119 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
-public class SomeSiteTest {
+public class MySiteTest {
 
 	private static WebDriver driver;
 	WebElement element;
 
 	@BeforeClass
 	public static void driverSetup() {
-		// ChromeDrirver, FireforxDriver, ...
-		System.setProperty("webdriver.chrome.driver", "/Users/Андрей/Downloads/chromedriver/chromedriver");
-		driver = new ChromeDriver();
+		ChromeOptions chromeOptions= new ChromeOptions();
+		chromeOptions.setBinary("D:\\Programs/Google/Chrome/Application/chrome.exe");
+		System.setProperty("webdriver.chrome.driver", "/Users/Андрей/Downloads/chromedriver/chromedriver.exe");
+		driver = new ChromeDriver(chromeOptions);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@Test
-	public void homePage(){
-		driver.get("http://localhost/pro/login.php/");
+	public void homePage() throws InterruptedException{
+		driver.get("http://localhost/pro/WebAdverts/test.php");
+		Thread.sleep(2500);
 		
-		element = driver.findElement(By.linkText("Polsat"));
+		element = driver.findElement(By.id("logo"));
 		assertNotNull(element);
 	}
 	
 	@Test
-	public void loginPage(){
-		driver.get("http://localhost/pro/login.php/");
-		driver.findElement(By.linkText("")).click();
-		element = driver.findElement(By.linkText(""));
+	public void loginPage() throws InterruptedException{
+		driver.get("http://localhost/pro/WebAdverts/login.php");
+		Thread.sleep(2500);
+		
+		assertTrue(driver.getPageSource().contains("Please, type your IDs to log:"));
+	}
+	
+
+	@Test
+	public void Wronglogin() throws InterruptedException{
+		driver.get("http://localhost/pro/WebAdverts/login.php");
+		Thread.sleep(2000);
+		
+		WebElement id = driver.findElement(By.name("username"));
+		WebElement pass = driver.findElement(By.name("password"));
+		WebElement login = driver.findElement(By.id("log_but"));
+		
+		id.sendKeys("wrongLogin");
+	    pass.sendKeys("wrongPassword");
+	    login.click();
+	        
+	    assertTrue(driver.getPageSource().contains("The username or password you entered are not good."));
+	    Thread.sleep(2500);
+		
 		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 	    assertNotNull(screenshot);
 
 		try {
-			FileUtils.copyFile(screenshot, new File("/Users/Андрей/Tmp/login.png"));
+			FileUtils.copyFile(screenshot, new File("/Users/Андрей/Tmp/WrongLogin.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 			assertTrue(false);
 		}
 		
+	}
+	
+	@Test
+	public void logOutTest() throws InterruptedException{
+		driver.get("http://localhost/pro/WebAdverts/test.php");
+		Thread.sleep(1000);
+		
+		WebElement logout = driver.findElement(By.id("Logout"));
+		logout.click();
+		
+	    assertTrue(driver.getPageSource().contains("You have successfully been logged out."));
+	    Thread.sleep(1000);
+		
+		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	    assertNotNull(screenshot);
+
+		try {
+			FileUtils.copyFile(screenshot, new File("/Users/Андрей/Tmp/Logout.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void sessionTest() throws InterruptedException{
+		driver.get("http://localhost/pro/WebAdverts/login.php");
+		Thread.sleep(1000);
+		
+		WebElement id = driver.findElement(By.name("username"));
+		WebElement pass = driver.findElement(By.name("password"));
+		WebElement login = driver.findElement(By.id("log_but"));
+		
+		id.sendKeys("admin");
+	    pass.sendKeys("vfrcbv");
+	    login.click();
+	        
+	    WebElement toMain = driver.findElement(By.linkText("Main Page"));
+	    Thread.sleep(1000);
+	    
+	    toMain.click();
+	    
+	    assertTrue(driver.getPageSource().contains("Logged as: admin"));
+		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	    assertNotNull(screenshot);
+
+		try {
+			FileUtils.copyFile(screenshot, new File("/Users/Андрей/Tmp/LoggedIn.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
 	}
 
 	@AfterClass
