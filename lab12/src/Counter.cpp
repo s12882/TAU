4b1943fc6a6b8433f2a481259385390d380190fe
Andrii Slobodian <whooles96@gmail.com>
2017-06-20 10:09:43 +0200

    Update Counter.cpp

--- lab12/src/Counter.cpp
+++ lab12/src/Counter.cpp
@@ -1,8 +1,8 @@
 /*
  * Counter.cpp
  *
- *  Created on: 18 θών. 2017 γ.
- *      Author: ΐνδπει
+ *  Created on: 18 Γ¨ΓΎΓ­. 2017 Γ£.
+ *      Author: Γ€Γ­Γ¤Γ°Γ¥Γ©
  */
 
 #include "Counter.hpp"
@@ -61,12 +61,12 @@ TEST_CASE( "Ring set Test", "[counter]" ) {
 	Counter count1;
 	count1.setRing(2);
 	count1.setPosition(20);
-	count1.work();
-
-	REQUIRE(count1.getRing() == 2);
 	REQUIRE(count1.getPosition() == 20);
+	count1.work();
+	REQUIRE(count1.getPosition() == 2);
+	
 
-	_getch();
+	
  }
 
 TEST_CASE( "Ring work Test", "[counter]" ) {
@@ -74,12 +74,33 @@ TEST_CASE( "Ring work Test", "[counter]" ) {
 	Counter count1;
 	count1.setRing(2);
 	count1.setPosition(20);
-	count1.work();
-
-	REQUIRE(count1.getRing() == 2);
+	
 	REQUIRE(count1.getPosition() == 20);
+	count1.work();
+	REQUIRE(count1.getPosition() == 2);
+	
 	REQUIRE(count1.ring(18) == false);
 	REQUIRE(count1.ring(2) == true);
 
+	_getch();
  }
+ 
+ SCENARIO("Countdown") {
+    Counter count1;
+    
+	int start = 20;
+	int end = 2;
+	
+	count1.setRing(end);
+	count1.setPosition(start);
+	
+    GIVEN("Starting point at 'start'") {
+        REQUIRE(count1.getPosition != NULL);
+        WHEN("We start countdown") {
+            REQUIRE_NOTHROW(count1.work());
+            THEN("It should ring at 'end' value") {
+				REQUIRE(count1.ring(2) == true);
+            }
+        }
+    }
 
