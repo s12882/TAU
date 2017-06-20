566c9f194176b54f9837cb24c079d0f9f4d0c090
Andrii Slobodian <whooles96@gmail.com>
2017-06-20 10:11:38 +0200

    Update CounterTest.cpp

--- lab12/src/CounterTest.cpp
+++ lab12/src/CounterTest.cpp
@@ -14,26 +14,50 @@
 using namespace std;
 
 
-TEST_CASE( "Ring set Test", "[vector]" ) {
+TEST_CASE( "Ring set Test", "[counter]" ) {
 
 	Counter count1;
 	count1.setRing(2);
 	count1.setPosition(20);
-	count1.work();
-
-	REQUIRE(count1.getRing() == 2);
 	REQUIRE(count1.getPosition() == 20);
+	count1.work();
+	REQUIRE(count1.getPosition() == 2);
+	
+
+	
  }
 
-TEST_CASE( "Ring work Test", "[vector]" ) {
+TEST_CASE( "Ring work Test", "[counter]" ) {
 
 	Counter count1;
 	count1.setRing(2);
 	count1.setPosition(20);
-	count1.work();
-
-	REQUIRE(count1.getRing() == 2);
+	
 	REQUIRE(count1.getPosition() == 20);
+	count1.work();
+	REQUIRE(count1.getPosition() == 2);
+	
 	REQUIRE(count1.ring(18) == false);
 	REQUIRE(count1.ring(2) == true);
+
+	_getch();
  }
+ 
+ SCENARIO("Countdown") {
+    Counter count1;
+    
+	int start = 20;
+	int end = 2;
+	
+	count1.setRing(end);
+	count1.setPosition(start);
+	
+    GIVEN("Starting point at 'start'") {
+        REQUIRE(count1.getPosition != NULL);
+        WHEN("We start countdown") {
+            REQUIRE_NOTHROW(count1.work());
+            THEN("It should ring at 'end' value") {
+				REQUIRE(count1.ring(2) == true);
+            }
+        }
+    }
